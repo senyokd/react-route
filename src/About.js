8fe07dbbfe752b6d75836da21825a6a673b67663
Senyo Kodjo Dzantor <68573498+senyokd@users.noreply.github.com>
2020-08-15 08:01:44 +0000

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,9 +5,9 @@ const About = () => {
     return (
         <div className="container">
         <div className="w3-content" >
-        <p><img style={{float:"left"}} src={waakye2} width="170" /><b>Waakye</b>, a dish made from beans and rice, is a familiar sight to most Ghanaians. This simple, nutritious meal can be found on sale all over the country, from restaurants to street food stalls. But how did it actually come to be?</p> <p>Waakye(pronounced ‘waachay’) is a combination of red or black-eyed beans and rice, and it’s often served with salads, spaghetti, fried plantains, garri, meat and fish. It’s a delicacy all over the country, and Ghanaians frequently queue at stalls to buy it for their breakfast or lunch.
+        <p><img style={{float:"left"}} src={waakye2} width="170" /><b>Waakye</b>, a dish made from beans and rice, is a familiar sight to most Ghanaians. This simple, nutritious meal can be found on sale all over the country, from restaurants to street food stalls. But how did it actually come about?</p> <p>Waakye(pronounced ‘waachay’) is a combination of red or black-eyed beans and rice, and it’s often served with salads, spaghetti, fried plantains, gari, meat and fish. It’s a delicacy all over the country, and Ghanaians frequently queue at stalls to buy it for their breakfast or lunch.
 
-Waakye originated from northern Ghana, where rice and beans are staple food crops. However, it’s not clear which specific ethnic group is responsible for pioneering this national treasure. Depending on where it’s prepared, certain variations exist in its ingredients, like the ratio of rice to beans or the spiciness of the accompanying shitor (hot pepper sauce).</p> 
+Waakye originated from northern Ghana, where rice and beans are staple food crops. However, it’s not clear which specific ethnic group is responsible for pioneering this national treasure. Depending on where it’s prepared, certain variations exist in its ingredients, like the ratio of rice to beans or the spiciness of the accompanying shito (hot pepper sauce).</p> 
 
 <p>Waakye is affordable and easy to come by where ever you are in Ghana. Doctors even recommend you eat this at least once every two weeks (ps:Senyo is the Doctors)</p>
     </div>
